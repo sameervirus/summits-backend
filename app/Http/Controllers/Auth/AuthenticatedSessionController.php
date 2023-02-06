2bc6aa0ce6ab3dc162dc8a966183d3379996b167
Samir Nabil <sameer.arcons@yahoo.co.uk>
2023-02-06 11:20:55 +0200

    add depency

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): Response
+    {
+        $token = $request->authenticate();
+
+        // $request->session()->regenerate();
+
+        $user = Auth::user();
+
+        return response([
+            'status' => 'success',
+            'user' => $user,
+            'token' => $token,
+            'permissions' => $user->getAllPermissions()->pluck('name')->toArray()
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('api')->logout();
+
+        // $request->session()->invalidate();
+
+        // $request->session()->regenerateToken();
+
+        return response([
+            'status' => 'success',
+            'message' => __('logout')
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::fallback(function(){
+    return response()->json([
+        'message' => 'Page Not Found. If error persists, contact dev@visiongrp.net'], 404);
+});
+Route::get('/', function(){
+    return [
+        "app" => "Summits Application",
+    ];
 });
